d3eedb3f98879fe0dda69e7171f6c91660e74ea5
albog <alboghardau@gmail.com>
2019-10-21 14:41:38 +0300

    Sync

---
+++ src/main/java/views/RoomEditorController.java
@@ -0,0 +1,15 @@
+package views;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RoomEditorController implements Initializable {
+
+    @FXML
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}
